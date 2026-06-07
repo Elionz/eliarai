080269293480df163c191938b1d524910312d707
Eli'ar <epixai@gmail.com>
2026-06-07 11:15:49 +0300

    Progress more

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
+from google.genai import types
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -7,20 +10,27 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("API is missing!!")
 
-from google import genai
-
 client = genai.Client(api_key=api_key)
 
 
-
 def main():
-    print("Hello from eliar!")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.5-flash", contents=messages
     )
 
+    if response.usage_metadata is None:
+        raise RuntimeError("API missing...")
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print(response.text)
 
 

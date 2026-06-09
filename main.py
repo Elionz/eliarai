2353d298ef1861b695b34a19748dc29e348a8382
Eli'ar <epixai@gmail.com>
2026-06-09 14:29:48 +0300

    Added calculator

--- main.py
+++ main.py
@@ -16,22 +16,31 @@ client = genai.Client(api_key=api_key)
 def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
+
 
     messages: list[types.Content] = [
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
 
+    if args.verbose is True:
+        print(f"User prompt: {args.user_prompt}")
+
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages
     )
 
+
     if response.usage_metadata is None:
         raise RuntimeError("API missing...")
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    if args.verbose is True:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print(response.text)
+
 
 
 if __name__ == "__main__":

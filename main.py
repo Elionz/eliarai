edf1bad5f4435fe3a1729f6a1a77fb4315b1aba0
Eli'ar <epixai@gmail.com>
2026-06-18 15:05:04 +0300

    Added functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google.genai import types
 from google import genai
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -46,8 +46,24 @@ def main():
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
     if response.function_calls:
+        function_results = []
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose=args.verbose)
+
+            if not function_call_result.parts:
+                 raise Exception("Empty")
+            
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("No details found")
+
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("No data found")
+            
+            function_results.append(function_call_result.parts[0])
+            
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            
     else:
         print(response.text)
 
